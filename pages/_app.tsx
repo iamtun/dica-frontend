8910f1f883484e9631b790912445d336d26ae40e
iamtun <letuan19431791.iuh@gmail.com>
2023-01-06 09:42:36 +0700

    init app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Quicksand } from '@next/font/google'
+
+const inter = Quicksand({
+  weight: '500',
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+    <Component {...pageProps} />
+  </>
 }
